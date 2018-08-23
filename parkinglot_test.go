8f228a63f1abbf1d271ace2e750c772401f6ad1e
Karinna Iniguez <kngz@amazon.com>
2018-08-23 08:36:23 -0700

    Add TestNewLot function with assertions

--- parkinglot_test.go
+++ parkinglot_test.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewLot(t *testing.T) {
+	expectedRegularSpaces := 3
+	expectedADASpaces := 4
+	expectedTotalSpaces := expectedADASpaces + expectedRegularSpaces
+	l, r := newLot(expectedRegularSpaces, expectedADASpaces)
+
+	if len(l) != expectedTotalSpaces {
+		t.Errorf("Expected new lot of %v spaces.  but got %v", expectedTotalSpaces, len(l))
+	}
+
+	if reflect.TypeOf(r) != reflect.TypeOf("string") {
+		t.Errorf("Expected a response of string")
+	}
+
+}
